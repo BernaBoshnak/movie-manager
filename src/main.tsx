ab30c04b552fa7f8ad9d16cedca0eab2f6a49d73
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2024-01-15 07:40:18 +0200

    install prettier and eslint plugins, added configuration and settings for automatic formatting and code validation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './components/App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
