e06b4726258fb4da3da409e74d26a38a7b40c1c9
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2024-01-15 07:57:19 +0200

    install bootstrap and react-bootstrap including scss styles and added settings in the vite configuration for automating imports ordering; introduces the module aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  build: {
+    sourcemap: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/assets/scss'),
+    },
+  },
 })
